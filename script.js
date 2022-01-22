9f1f68dafd2a010233603c5d6501b6c45691a6f7
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-01-22 12:56:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function main() {
     let res = await fetch('https://forum-test.chiroyce.repl.co/');
     res = await res.json();
     document.getElementById("post").href = `https://scratch.mit.edu/discuss/post/${res.latest_post}`;
-    document.getElementById("post").innerText = 
+    document.getElementById("post").innerText = `https://scratch.mit.edu/discuss/post/${res.latest_post}`;
     document.title = `#${res.latest_post}`;
 }
 
