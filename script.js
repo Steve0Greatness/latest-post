7ca3e3fe51e038d8c339baebeb0a53731be130d4
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-01-22 11:53:09 +0530

    Run the function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 async function main() {
     let res = await fetch('https://forum-test.chiroyce.repl.co/');
     res = await res.json();
-    location.replace(`https://scratch.mit/edu/post/${res.latest_post}`)
+    location.replace(`https://scratch.mit/edu/post/${res.latest_post}`);
 }
+
+main();
