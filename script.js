9c2ef100c3191ea0d00a88107d18fa83ceafe6cd
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-01-22 11:52:05 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+async function main() {
+    let res = await fetch('https://forum-test.chiroyce.repl.co/');
+    res = await res.json();
+    location.replace(`https://scratch.mit/edu/post/${res.latest_post}`)
+}
